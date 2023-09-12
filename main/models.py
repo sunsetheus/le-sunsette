9cf8947254578417a48359d0e346cec5c75d2bf3
sunsetheus <williamsalimrjs@gmail.com>
2023-09-12 07:22:26 +0700

    set up djago project

---
+++ main/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Item(models.Model):
+  name = models.CharField(max_length=255)
+  amount = models.IntegerField()
+  description = models.TextField()
+  price = models.IntegerField()
+  type = models.CharField(max_length=255)
---
+++ main/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+def show_main(request):
+  context = {
+     'nama_mahasiswa': 'William',
+     'kelas_mahasiswa': 'PBP D',
+     'nama_aplikasi': 'le-sunsette',
+     'name': 'Plain croissant',
+     'amount': 6,
+     'description': 'Classic type croissant with layers of buttery dough, each bite reveals a harmony of flaky and melted buttery. Savor the simplicity of pure delight – the perfect companion to your morning coffee or a snack',
+     'price': 35000
+  }
+
+  return render(request, "main.html", context)
